b9c0b4fccc112c8b624e22bc97b909729e13cb32
Camille Franceschi <franceschi.camille@gmail.com>
2017-05-22 13:16:35 +0200

    reponsive sidebar to nav bar

--- source/javascripts/sidebar.js
+++ source/javascripts/sidebar.js
@@ -11,31 +11,50 @@
 // });
 
 
-// $(document).ready(function() {
-
-//   $(window).resize(function () {
-//     var viewportWidth = $(window).width();
-//     if (viewportWidth < 768) {
-//       console.log("test")
-//       $("#wrapper").removeClass("toggled");
-//       $('#sidebar').toggle("slow", function() { // enleve le contenu et le remet hide and show
-//         console.log("callback");
-//         $("#page-content").toggleClass("toggled");
-//         // $('#menu').css('left', '0')  // modifie de maniere permanente
-//       });
-//     } else {
-//       $( "#menu-toggle" ).on("click", function(e) {
-//           e.preventDefault();
-//           console.log("test");
-//           $("#wrapper").toggleClass("toggled");
-//       });
-//     }
-//   });
-// });
 $(document).ready(function() {
-  $( "#menu-toggle" ).on("click", function(e) {
+
+  var viewportWidth = $(window).width();
+  if (viewportWidth < 768) {
+    console.log("small screen 1");
+    $("#wrapper").removeClass("toggled");
+    $(".navbar-resume-item").show();
+    $("#menu-toggle").hide();
+    $(".sidebar-nav").hide();
+  } else {
+    console.log("bigger screen 1");
+    $("#menu-toggle").show();
+    $(".navbar-resume-item").hide();
+    $(".sidebar-nav").show();
+    $("#wrapper").addClass("toggled");
+    $( "#menu-toggle" ).on("click", function(e) {
+      console.log("click general1");
       e.preventDefault();
-      console.log("test");
       $("#wrapper").toggleClass("toggled");
+    });
+  };
+
+
+
+  $(window).resize(function () {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 768) {
+      console.log("small screen 2");
+      $("#wrapper").removeClass("toggled");
+      $(".navbar-resume-item").show();
+      $("#menu-toggle").hide();
+      $(".sidebar-nav").hide();
+    } else {
+      console.log("bigger screen 2 ");
+      $("#menu-toggle").show();
+      $(".navbar-resume-item").hide();
+      $(".sidebar-nav").show();
+      $("#wrapper").addClass("toggled");
+      $( "#menu-toggle" ).on("click", function(e) {
+        console.log("click general2");
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+      });
+    };
   });
 });
+
