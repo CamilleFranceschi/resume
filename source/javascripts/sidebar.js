861f19104fe963861a1544d9f133ed5ee3c56b73
Camille Franceschi <franceschi.camille@gmail.com>
2017-05-22 21:20:03 +0200

    worked on responsive side projects

--- source/javascripts/sidebar.js
+++ source/javascripts/sidebar.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
 
   var viewportWidth = $(window).width();
@@ -9,12 +7,13 @@ $(document).ready(function() {
     $(".navbar-resume").show();
     $("#menu-toggle").hide();
     $(".sidebar-nav").hide();
-    $(".product-logo").hide();
+    $(".product-logo").detach();
   } else {
     console.log("bigger screen 1");
     $("#menu-toggle").show();
     $(".navbar-resume").hide();
     $(".sidebar-nav").show();
+    $(".product-logo").append();
     $("#wrapper").addClass("toggled");
     $( "#menu-toggle" ).on("click", function(e) {
       console.log("click general1");
@@ -25,13 +24,14 @@ $(document).ready(function() {
 
   $(window).resize(function () {
     var viewportWidth = $(window).width();
+    var logo = $(".product-logo").clone(true);
     if (viewportWidth < 768) {
       console.log("small screen 2");
       $("#wrapper").removeClass("toggled");
       $(".navbar-resume").show();
       $("#menu-toggle").hide();
       $(".sidebar-nav").hide();
-      $(".product-logo").hide();
+      $(".product-logo").detach();
       $(".hamburger").removeClass("is-open");
       $(".hamburger").addClass("is-closed");
     } else {
@@ -39,6 +39,7 @@ $(document).ready(function() {
       $("#menu-toggle").show();
       $(".navbar-resume").hide();
       $(".sidebar-nav").show();
+      logo.appendTo(".test");
       $(".hamburger").removeClass("is-open");
       $(".hamburger").addClass("is-closed");
       $("#wrapper").addClass("toggled");
